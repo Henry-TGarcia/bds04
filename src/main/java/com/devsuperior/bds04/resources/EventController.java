f484fdc6dc2ff728016b86a1939f1400638b1232
Henry T. Garcia <henrytgarcia@uol.com.br>
2021-07-28 11:40:32 -0300

    CRUD of events, cities and user

---
+++ src/main/java/com/devsuperior/bds04/resources/EventController.java
@@ -0,0 +1,38 @@
+package com.devsuperior.bds04.resources;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+	@Autowired
+	private EventService service;
+
+	@GetMapping
+	public ResponseEntity<List<EventDTO>> findAll(){
+		List<EventDTO> lista = service.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
+	
+	@PostMapping
+	public ResponseEntity<EventDTO> insert(@RequestBody EventDTO dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+		.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+}
